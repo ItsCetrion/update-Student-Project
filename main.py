cfca258e2955ebdf1e3ead847a7a90fcae15be7b
79951 <goodplay2.0@mail.ru>
2023-09-09 11:24:58 +0700

    создание объекта data из строки

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 dict_ = {1:"Января", 2:"Февраля", 3:"Марта", 4:"Апреля", 5:"Мая", 6:"Июня", 7:"Июля", 8:"Августа", 9:"Сентября", 10:"Октября", 11:"Ноября", 12:"Декабря"}
 class Currency:
     "Информация о валюте в рублях"
-    def __init__(self, FirstRate: str, SecondRate: str, rate: float, data: str) -> None:
-        data = data.split(".")
-        self.FirstRate, self.SecondRate, self.rate, self.data = FirstRate, SecondRate, rate, Data(data[0], data[1], data[2])
+    def __init__(self, FirstRate: str, SecondRate: str, rate: float, data) -> None:
+        self.FirstRate, self.SecondRate, self.rate, self.data = FirstRate, SecondRate, rate, data
 class Data:
-    def __init__(self, day: str, month: str, year: str):
-        self.day = int(day)
-        self.month = int(month)
-        self.year = int(year)
+    def __init__(self, data: str):
+        data = data.split(".")
+        self.day = int(data[0])
+        self.month = int(data[1])
+        self.year = int(data[2])
+    def __str__(self):
+        return f"{self.day} {dict_.get(self.month)} {self.year}"
 
 def file_operations() -> None:
     with open("test.txt") as file:
@@ -24,9 +26,11 @@ def file_operations() -> None:
             data = file.readline()
             if data[-1] == "\n":
                 data = data[:-1]
+
+            data = Data(data)
             CurrencyInformation.append(Currency(FirstRate, SecondRate, rate, data))
     for i in CurrencyInformation:
-        print(f"Курс {i.SecondRate} на {i.FirstRate} составляет {i.rate} на {i.data.day} {dict_.get(i.data.month)} {i.data.year}")
+        print(f"Курс {i.SecondRate} на {i.FirstRate} составляет {i.rate} на {i.data}")
 file_operations()
 
 
